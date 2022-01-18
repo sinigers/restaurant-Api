de980ad2433fbdb59ce54d990ed381ffb9b27e01
Alexander Sinigerov <64265892+sinigers@users.noreply.github.com>
2022-01-18 11:23:21 +0200

    productController router.get("/:productId/delete" router.delete(...

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -39,7 +39,7 @@ router.patch("/:productId/edit", auth, async (req, res, next) => {
   }
 });
 
-router.delete("/:productId/delete", async (req, res) => {
+router.get("/:productId/delete", async (req, res) => {
   try {
     let deleted = await productService.delete(req.params.productId);
     res.send(deleted);
@@ -68,3 +68,79 @@ router.post("/create", async (req, res) => {
 });
 
 module.exports = router;
+
+// const { isAuth }  = require("../middlewares/authMiddleware.js");
+// const router = require("express").Router();
+
+// const productsService = require("../services/productsService");
+
+// router.post("/create", async (req, res) => {
+//   try {
+//     await productsService.create({ ...req.body });
+//     res.send(req.body);
+//   } catch (error) {
+//     res.status(500).end();
+//   }
+// });
+
+// router.get("/:productId/details", async (req, res) => {
+//   try {
+//     let product = await productsService.getOne(req.params.productId);
+//     res.send(product);
+//   } catch (error) {
+//     res.status(404).send(error);
+//   }
+// });
+
+// router.get("/products", async (req, res) => {
+//   try {
+//     let products = await productsService.getAll();
+//     res.send(products);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
+
+// router.get("/:productId/delete", async (req, res) => {
+//   try {
+//     let deleted = await productsService.delete(req.params.productId);
+//     res.send(deleted);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
+
+// router.post("/:productId/edit", async (req, res) => {
+//   try {
+//     let product = await productsService.updateOne(
+//       req.params.productId,
+//       req.body
+//     );
+//     res.send(product);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
+
+// router.get("/catalog/category/:categoryName", async (req, res) => {
+//   try {
+//     let product = await productsService.getByCategory(req.params.categoryName);
+//     res.send(product);
+//   } catch (error) {
+//     res.status(404).send(error);
+//   }
+// });
+
+// router.get("/catalog/category/:categoryName/:productId", async (req, res) => {
+//   try {
+//     let products = await productsService.getByCategorySix(
+//       req.params.categoryName,
+//       req.params.productId
+//     );
+//     res.send(products);
+//   } catch (error) {
+//     res.status(404).send(error);
+//   }
+// });
+
+// module.exports = router;
